aebbadd1bb0ab364d8c048a7db79ac321f42b097
Bakshish Singh <bakshish10621@gmail.com>
2023-10-08 22:24:59 +0530

    dhanraj push

--- client/src/components/Card2.jsx
+++ client/src/components/Card2.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Card2 = ({link,title,description, imageUrl }) => {
   return (
-    <div className="w-[23rem] h-[16rem] rounded-[2rem] border-2 border-slate-200 overflow-hidden mx-4 my-4">
+    <div className="w-[23rem] h-[16rem] rounded-[2rem] border-2 border-teal-200 overflow-hidden mx-4 my-4">
       <img className="h-[2rem] w-[2rem] mt-6 ml-8" src={imageUrl} alt="" />
       <div className=' font-semibold text-3xl m-3 ml-6'>{title}</div>
       <div className="px-6 py-4">
         <p className="text-gray-700 text-2xl">{description}</p>
       </div>
-      <button className=' text-blue-500 font-extrabold ml-6'>{link}</button>
+      <button className=' text-teal-500 font-extrabold ml-6'>{link}</button>
     </div>
   );
 };
--- client/src/components/Section2.jsx
+++ client/src/components/Section2.jsx
@@ -15,27 +15,27 @@ const Section2 = () => {
       </div>
       <div className="buttons m-4">
         <button
-          className={`text-2xl border-2 border-gray-200 m-4 p-4 rounded-full transition-all duration-300 ease-out ${activeDisplay === 'display1' ? 'text-blue-500 border-blue-500' : 'text-gray-500'}`}
+          className={`text-2xl border-2 border-teal-200 m-4 p-4 rounded-full transition-all duration-300 ease-out ${activeDisplay === 'display1' ? 'text-teal-500 border-teal-500 bg-teal-100' : 'text-teal-500'}`}
           onClick={() => handleButtonClick('display1')}
         >
           Stocks and ETFs
         </button>
         <button
-          className={`text-2xl border-2 border-gray-200 m-4 p-4 rounded-full transition-all duration-300 ease-out ${activeDisplay === 'display2' ? 'text-blue-500 border-blue-500' : 'text-gray-500'}`}
+          className={`text-2xl border-2 border-teal-200 m-4 p-4 rounded-full transition-all duration-300 ease-out ${activeDisplay === 'display2' ? 'text-teal-500 border-teal-500 bg-teal-100' : 'text-teal-500'}`}
           onClick={() => handleButtonClick('display2')}
         >
           Mutual Funds
         </button>
         <button
-          className={`text-2xl border-2 border-gray-200 m-4 p-4 rounded-full transition-all duration-300 ease-out ${activeDisplay === 'display3' ? 'text-blue-500 border-blue-500' : 'text-gray-500'}`}
+          className={`text-2xl border-2 border-teal-200 m-4 p-4 rounded-full transition-all duration-300 ease-out ${activeDisplay === 'display3' ? 'text-teal-500 border-teal-500 bg-teal-100' : 'text-teal-500'}`}
           onClick={() => handleButtonClick('display3')}
         >
           US Stocks
         </button>
       </div>
-      <div className={`display1 border-2 border-gray-200 rounded-lg h-[30rem] w-[75rem] bg-gradient-to-r from-white via-white to-blue-100 transition-all duration-300 ease-in ${activeDisplay === 'display1' ? 'opacity-100' : 'hidden'}`}></div>
-      <div className={`display2 border-2 border-gray-200 rounded-lg h-[30rem] w-[75rem] bg-gradient-to-r from-white via-white to-slate-100 transition-all duration-300 ease-in ${activeDisplay === 'display2' ? 'opacity-100' : 'hidden'}`}></div>
-      <div className={`display3 border-2 border-gray-200 rounded-lg h-[30rem] w-[75rem] bg-gradient-to-r from-white via-white to-yellow-100 transition-all duration-300 ease-in ${activeDisplay === 'display3' ? 'opacity-100' : 'hidden'}`}></div>
+      <div className={`display1 border-2 border-teal-200 rounded-lg h-[30rem] w-[75rem] bg-gradient-to-r from-white via-white to-teal-100 transition-all duration-300 ease-in ${activeDisplay === 'display1' ? 'opacity-100' : 'hidden'}`}></div>
+      <div className={`display2 border-2 border-teal-200 rounded-lg h-[30rem] w-[75rem] bg-gradient-to-r from-white via-white to-slate-100 transition-all duration-300 ease-in ${activeDisplay === 'display2' ? 'opacity-100' : 'hidden'}`}></div>
+      <div className={`display3 border-2 border-teal-200 rounded-lg h-[30rem] w-[75rem] bg-gradient-to-r from-white via-white to-yellow-100 transition-all duration-300 ease-in ${activeDisplay === 'display3' ? 'opacity-100' : 'hidden'}`}></div>
     </div>
   );
 };
