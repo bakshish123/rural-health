f45875d5420247ee734ada48419dac9545d3e6fa
Bakshish Singh <bakshish10621@gmail.com>
2023-10-09 20:09:30 +0530

    Your commit message here

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
-import Invest from "./pages/Invest";
 import Pricing from "./pages/Pricing";
+import BookApp from "./pages/BookApp";
 import Learn from "./pages/Learn";
-import Blogs from "./pages/Blogs";
+// import Blogs from "./pages/Blogs";
 import Support from "./pages/Support";
+// import BookApp from "./pages/BookApp";
 
 const App = () => {
   return (
@@ -17,10 +18,10 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/invest" element={<Invest />} />
           <Route path="/pricing" element={<Pricing />} />
+          <Route path="/bookAppointment" element={<BookApp />} />
           <Route path="/learn" element={<Learn />} />
-          <Route path="/blogs" element={<Blogs />} />
+          {/* <Route path="/blogs" element={<Blogs />} /> */}
           <Route path="/support" element={<Support />} />
         </Routes>
       </Router>
--- client/src/components/Card4.jsx
+++ client/src/components/Card4.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
-const Card4 = () => {
+import React from 'react';
+import { Link } from "react-router-dom";
+const Card = ({description, imageUrl,link }) => {
   return (
-    <div>Card4</div>
-  )
-}
+    <div className="w-[23rem] h-[12rem] rounded-[2rem] border-2 border-teal-200 overflow-hidden mx-4 my-4">
+      <img className="h-[2rem] w-[2rem] mt-6 ml-8" src={imageUrl} alt="" />
+      <div className="px-6 py-4">
+        <p className="text-gray-700 text-2xl">{description}</p>
 
-export default Card4
+      </div>
+      <div className="">
+        <Link to='/support'>
+          <button >
+            <span>
+              <p className="ml-5  mt-4 text-2xl text-teal-600 hover:text-teal-700">{link}</p>
+            </span>
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+export default Card;
